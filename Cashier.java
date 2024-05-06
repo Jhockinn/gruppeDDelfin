d8d1b2d2502ba463e554bb3f2c1668d114fb7a13
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-05-06 10:43:45 +0200

    Casher UI muligheder

--- Cashier.java
+++ Cashier.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class Cashier{
 
-}
+/*
+to-do: 
+        see memberlist
+        see kontigent
+        see restance
+            add people to restance list?
+            remove people?
+*/
+
+    while (true) {
+        System.out.println("\n");
+        System.out.println("Cashier UI Options:");
+        System.out.println("1: See memberlist");
+        System.out.println("2: Late payments acess");
+        System.out.println("3: Go back to the main menu");
+        System.out.println("\n")
+    }
+    int choice = scanner.nextInt();
+
+    switch(choice) {
+        case 1: //access memberlist (Niko G + Tobias)
+
+        break;
+
+        case 2: //restance (Kristoffer -- WORK IN PROGRESS
+
+        break;
+
+        case 3: //go back to main menu
+
+        default: 
+        System.out.println("Invalid input");
+        break;
+    }
+
+    }
+
