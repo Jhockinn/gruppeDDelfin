b8a2cca094e3d618b75610b29789b4afa4bc87fb
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-05-06 11:23:09 +0200

    payment UI. to do: see list (generate list?), add people to the list, remove people from the list

---
+++ Payments.java
@@ -0,0 +1,27 @@
+public class Payments{
+
+
+    System.out.println("\n");
+    System.out.println("Options:");
+    System.out.println("1: See list of people with missing payments");
+    System.out.println("2: Add people to the list of missing payments");
+    System.out.println("3: Remove people from the list of missing payments");
+    System.out.println("\n");
+
+/*
+TO DO: see restance
+        see restance amounts
+        add people to restance
+
+
+
+
+*/
+
+
+
+
+
+
+
+}
